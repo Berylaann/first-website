4bc2f70640a8a79adf0d5ef41562ce76212894ec
Beryl Ann <65688867+Berylaann@users.noreply.github.com>
2024-05-22 18:11:04 +0100

    project filees added

--- index.js
+++ index.js
@@ -60,3 +60,98 @@ addition(2300, 40000);
 
 let additionOfTwoNumbers = addition(1000, -2); // 998
 console.log(additionOfTwoNumbers);
+
+function newConversation(person, topic) {
+    console.log("Hey, " + person + "!");
+    console.log("How are you?");
+    console.log("Do you like" + topic + "?");
+}
+
+newConversation("Shakirah", "programming");
+newConversation("Varlene", "HTML");
+
+// function expression
+const multiply = function(num) {
+    return num * num;
+    /* working with multiple lines of code inside return statement
+    return {
+        let total = num * num
+    }; */
+};
+
+console.log (multiply(2));
+
+// local scope: variables created inside a function only works in the function
+// you cannot access the myName variable outside of the function
+
+function testing() {
+    const myName = "Beryl Ann";
+    console.log(myName)
+}
+
+testing()
+
+// this will throw an error because the variable cannot be accessed outside of the function
+//console.log(myName);
+
+let age = 12;
+
+age = 30;
+
+console.log(age);
+
+// an immediately-involved function expression
+(function () {
+// code to execute
+console.log("running now")
+} ())
+
+// function expression
+
+/* const multiply = function (num) {
+    return num * num;
+} */
+
+// an arrow function
+const multiply2 = (num) => {
+    return num * num;
+  }
+  
+  const multiply3 = num => num * num;
+
+  const person = {
+    firstName: "Sakirat",
+    lastName: "Usman",
+    likes: "Pizza",
+  };
+
+  // dot notation
+  console.log(person.firstName);
+
+  // bracket notation
+  console.log(person["lastName"]);
+
+  person.likes = "Shawarma";
+
+  console.log(person)
+// Example
+
+const personA = {
+    firstName: 'Monique',
+    likes: 'pizza'
+};
+
+const personB = personA;
+
+console.log("Before");
+console.log(personA.firstName);
+console.log(personB.firstName);
+
+personA.firstName = "Stacy";
+
+console.log("After");
+console.log(personA.firstName);
+console.log(personB.firstName);
+
+
+
